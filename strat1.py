dde54830a64fcaa0fe3ebd03321c5f382624782c
Jack Baer <jackbaer@mac.mines.edu>
2025-11-06 14:35:43 -0700

    Added more skeleton code to strat1.py

--- strat1.py
+++ strat1.py
@@ -4,6 +4,10 @@ import sys
 '''
 Read file input/Create Board
 '''
+# At global scope, read in all file data into 3 variables:
+# r - row count
+# c - column count
+# grid - 2D list of cells, with integers representing cell colors
 r = None
 c = None
 grid = []
@@ -31,24 +35,22 @@ print(f'{r} {c}')
 print(grid)
 
 '''
-Global variables
-'''
-moves = []
-score = 0
-
-'''
 Helper method for identifying possible moves at given board state
 '''
 # Find clusters using adjacent cells
+# Takes in a given board state
 # If a cluster has size >= 2, add it to a list of possible moves
+def find_clusters(board):
+
 
 
 '''
 Helper method for making a move/updating board
 '''
-# Take in a board and a move from our list
+# Take in a board state and a cluster to delete
 # Remove cluster from board, apply game rules as needed (gravity/left-shifting)
 # Return new board state
+def remove_cluster(board, cluster):
 
 '''
 Tree search method
@@ -56,6 +58,7 @@ Tree search method
 # Build decision tree for a given board state
 # Use helper method to generate list of all possible moves
 # Create a child node for every move, and use helper method to get new board state/score increase
+def build_tree(board, depth, alpha):
 
 '''
 Rolling horizon approach
@@ -63,12 +66,41 @@ Rolling horizon approach
 # Loop through board, beginning with starting position
 # In every iteration, build/traverse decision tree to find best path(s) at a certain hard-coded depth
 # Feed best options back into tree search until end state is found (helper method fails to return any possible moves)
+def run_game(board):
+    while True:
+        if(find_clusters() == 0):
+            break;
+        else:
+
+
+'''
+Helper method to determine points scored from final moves list
+'''
+# In essence, loop through final path and calculate cumulative score
+# (Get points for each move with formula (n-1)^2)
+# Each move has cluster size as second value
+def determine_score(moves):
+    score = 0
+
+    for m in moves:
+        _, cluster_size, _, _ = m
+        move_score = (cluster_size - 1) ** 2
+        score += move_score
+
+    return score
 
 '''
 Output to console
 '''
+moves = [] # List of moves in selected path
+score = 0 # Number of points awarded in decision path
+
+moves = run_game(grid)
+
+score = determine_score(moves)
+
+print(score)
 print(len(moves))
-# All subsequent lines are each individual movement Rocket and Lucky take
 for m in moves:
     print(m)
 
