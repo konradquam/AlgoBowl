c0c752b51abb8bbf631c469c54d1eef58c6dacbe
Jack Baer <jackbaer@JackBook-Pro.local>
2025-11-06 19:25:53 -0700

    Updated find_clusters() and wrote first component of remove_cluster(), basic tests appear to work. Still need to implement left-shift behavior for updating board when removing a cluster

--- strat1.py
+++ strat1.py
@@ -11,7 +11,7 @@ Read file input/Create Board
 # note that 0's represent empy cells
 r = None
 c = None
-grid = []
+STARTING_BOARD = []
 
 if len(sys.argv) < 2:
     print("Usage: python3 strat1.py <filename>")
@@ -28,13 +28,13 @@ with open(filename, "r") as file:
         curr_row = []
         for char in cleaned_line:
             curr_row.append(int(char))
-        grid.append(curr_row)
+        STARTING_BOARD.append(curr_row)
 
 # convert grid to numpy
-grid = np.array(grid)
+STARTING_BOARD = np.array(STARTING_BOARD)
 
 print(f'{r} {c}')
-print(grid)
+print(STARTING_BOARD)
 
 '''
 Helper method for identifying possible moves at given board state
@@ -43,12 +43,26 @@ Helper method for identifying possible moves at given board state
 # Takes in a given board state
 # If a cluster has size >= 2, add it to a list of possible moves
 def find_clusters(board):
-    clusters = set()
+    visited = set()
+    clusters = []
+    
+    # Iterate through cells in board to group together into clusters
     for i in range(r):
         for j in range(c):
-            adjacent = group_adjacent(board, r, c, i, j)
-            if len(adjacent) >= 2 and adjacent not in clusters:
-                clusters.add(adjacent)
+            # Skip any cells that have already been visited, meaning it's in another cluster
+            # Also skip any empty cells
+            if (i, j) in visited or board[i, j] == 0:
+                continue;
+
+            # Form cluster for current cell if it isn't part of a cluster
+            cluster = group_adjacent(board, r, c, i, j)        
+            for cell in cluster:
+                visited.add(cell)
+
+            # If cluster contains 2 or more cells, it's a valid move - add to list
+            if len(cluster) >= 2:
+                clusters.append(cluster)
+
     return clusters
 
 
@@ -59,14 +73,46 @@ Helper method for making a move/updating board
 # Remove cluster from board, apply game rules as needed (gravity/left-shifting)
 # Return new board state
 def remove_cluster(board, cluster):
+    # Copy board to modify (have to be explicit, since otherwise it's just a reference)
+    modified_board = board.copy()
 
+    # Remove cluster (set all cells to '0')
+    for (i, j) in cluster:
+        modified_board[i, j] = 0
+
+    # Apply gravity to cells (make nonzero cells above '0' cells fall down)
+    # Done by iterating through columns from the bottom-up
+    for j in range(0, c):
+        nonzero_cells = []
+        for i in range(0, r):
+            # If cell is nonzero, add to list
+            if modified_board[i, j] != 0:
+                nonzero_cells.append(modified_board[i, j])
+
+        # Track row in current column to replace tiles
+        current_row = r - 1
+        
+        # Rebuilding column from the bottom-up
+        for cell in reversed(nonzero_cells):
+            modified_board[current_row, j] = cell
+            current_row -= 1
+
+        # Once all nonzero cells have been placed, fill rest of column with '0'
+        for i in range(0, current_row + 1):
+            modified_board[i, j] = 0
+
+    # Apply 'left-shift' for columns
+    # WORK IN PROGRESS
+
+    return modified_board
+ 
 '''
 Tree search method
 '''
 # Build decision tree for a given board state
 # Use helper method to generate list of all possible moves
 # Create a child node for every move, and use helper method to get new board state/score increase
-def build_tree(board, depth, alpha):
+#def build_tree(board, depth, alpha):
 
 '''
 Rolling horizon approach
@@ -74,12 +120,13 @@ Rolling horizon approach
 # Loop through board, beginning with starting position
 # In every iteration, build/traverse decision tree to find best path(s) at a certain hard-coded depth
 # Feed best options back into tree search until end state is found (helper method fails to return any possible moves)
+'''
 def run_game(board):
     while True:
         if(find_clusters() == 0):
             break;
         else:
-
+'''
 
 '''
 Helper method to determine points scored from final moves list
@@ -87,6 +134,7 @@ Helper method to determine points scored from final moves list
 # In essence, loop through final path and calculate cumulative score
 # (Get points for each move with formula (n-1)^2)
 # Each move has cluster size as second value
+'''
 def determine_score(moves):
     score = 0
 
@@ -96,9 +144,12 @@ def determine_score(moves):
         score += move_score
 
     return score
+'''
 
 '''
 Output to console
+'''
+
 '''
 moves = [] # List of moves in selected path
 score = 0 # Number of points awarded in decision path
@@ -111,7 +162,7 @@ print(score)
 print(len(moves))
 for m in moves:
     print(m)
-
+'''
 
 
 '''
@@ -120,8 +171,10 @@ Builds adjacency set of same color, only along rows and columns, not diaganols a
 def group_adjacent(board, r, c, i, j):
     visited = set()
     adjacent = set()
-    #only find ajacent indices for possible colors 1-8
-    if 1<= board[i, j] <= 8:
+    # Only find ajacent indices for possible colors 1-8
+    if 1 <= board[i, j] <= 8:
+        # Add initial cell, then find all adjacent ones with helper method
+        adjacent.add((i, j)) 
         adjacency_helper(board, r, c, i, j, visited, adjacent)
     return adjacent
 
@@ -147,3 +200,28 @@ def adjacency_helper(board, r, c, i, j, visited, adjacent):
         if board[i, j+1] == board[i, j]:
             adjacent.add((i, j+1))
             adjacency_helper(board, r, c, i, j+1, visited, adjacent)
+
+
+# --- TESTING ---
+clusters = find_clusters(STARTING_BOARD)
+print(f"Found {len(clusters)} clusters:")
+for idx, cl in enumerate(clusters):
+    print(f"  Cluster {idx + 1} (size {len(cl)}): {cl}")
+
+new_board =  None
+# Try removing the first cluster
+if clusters:
+    print("\nRemoving first cluster")
+    new_board = remove_cluster(STARTING_BOARD, clusters[0])
+    print(new_board)
+
+if clusters:
+    print("\nRemoving fifth cluster")
+    new_board = remove_cluster(new_board, clusters[4])
+    print(new_board)
+
+print("\n\nBUG TO FIX - Clusters have to be recalculated after moving, since this just tries to delete the cluster from it's old space (which is already empty)")
+if clusters:
+    print("\nRemoving third cluster")
+    new_board = remove_cluster(new_board, clusters[2])
+    print(new_board)
